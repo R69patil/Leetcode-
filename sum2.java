a5465d4276a606c25a0059e63ffd52da40d0bc0d
Rahul Banappagouda Patil <98758481+R69patil@users.noreply.github.com>
2024-09-02 08:05:06 +0530

    1 problem

---
+++ sum2.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class twosum{
+    public static  void main(String[],args)
+    {
+        int[] arr ={11,3,7,9,14,2};
+        int target =17;
+        int[] ans =new int[2];
+
+
+        HashMap
+    }
+}
